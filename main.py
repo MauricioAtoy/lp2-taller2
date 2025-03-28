9527be250005737f49615fe0b87c4dd7fe26e5c9
JavierLlanos <javierllanosatoy@gmail.com>
2025-03-27 22:10:21 -0500

    Subiendo cambios

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template
 import sqlite3
 from pprint import pprint
 
 # cargamos todos los datos
-
+conexion = sqlite3.connect('web2.sqlite3')
+conexion.row_factory = sqlite3.Row #modo diccionario
+cursor = conexion.cursor()
+cursor.execute(""""
+SELECT * FROM productos;
+""")
+productos = [ dict(producto) for producto in cursor.fetchall()]
+print(productos)
+cursor.close()
+conexion.close()
 
 # aplicación
 app = Flask(__name__)
@@ -11,6 +20,7 @@ app = Flask(__name__)
 # rutas
 @app.route('/')
 def ruta_raiz():
+  return render_template('index.html', productos=productos)
   pass
 
 @app.route('/producto/<int:pid>')
