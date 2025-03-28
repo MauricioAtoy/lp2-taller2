79629a2192afdc99d07f42541172f0362cc5a50f
JavierLlanos <javierllanosatoy@gmail.com>
2025-03-27 22:42:55 -0500

    Subiendo cambios

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, redirect
 import sqlite3
 from pprint import pprint
 
@@ -20,12 +20,14 @@ app = Flask(__name__)
 @app.route('/')
 def ruta_raiz():
   return render_template('index.html', productos=productos)
-  pass
 
 @app.route('/producto/<int:pid>')
 def ruta_producto(pid):
-  pass
-  
+  for producto in productos:
+    if pid == producto.id:
+      return render_template('producto.html', producto=producto)
+  return redirect('/')
+
 # programa principal
 if __name__ == '__main__':
   app.run(host='0.0.0.0', debug=True)
